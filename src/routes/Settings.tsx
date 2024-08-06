0cf05345cf28a0445af2ae07c766fe7f99049075
Lewis Davidson <lewisdavidson8@gmail.com>
2024-08-06 03:08:20 +0100

    Setup Theme css
    Add Theme switcher to Settings page.

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,5 +1,35 @@
 import type { Component } from "solid-js";
+import { For } from "solid-js";
+import { Button } from "~/components/ui/button";
+import { themes } from "~/config/themeConfig";
+import { useTheme } from "~/context/ThemeProvider";
+import { cn } from "~/lib/utils";
 
 export const Settings: Component = () => {
-	return <h1>Settings</h1>;
+	const { theme, setTheme } = useTheme();
+	return (
+		<div class="flex flex-col max-w-4xl mx-auto">
+			<div>
+				<h2>Theme</h2>
+				<For each={themes}>
+					{(themeName) => (
+						<Button
+							variant="outline"
+							onClick={() => setTheme(themeName)}
+							class={cn(
+								"capitalize text-foreground",
+								theme() === themeName && "bg-accent",
+							)}
+						>
+							{themeName}
+						</Button>
+					)}
+				</For>
+			</div>
+			<div>
+				<h2>Min Speed</h2>
+				<p>Automatically fails a test if your speed falls below a threshold.</p>
+			</div>
+		</div>
+	);
 };
