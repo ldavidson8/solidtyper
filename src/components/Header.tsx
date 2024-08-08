502f7676d44b8308ff8ef891c89a039b37797966
Lewis Davidson <lewisdavidson8@gmail.com>
2024-08-08 02:04:45 +0100

    Adjust css to use [data-theme]
    fixing Header alignment of nav buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import InfoIcon from "~icons/lucide/info";
 
 export const Header = () => {
 	return (
-		<nav class="max-w-6xl w-full px-6 h-[72px] flex items-center justify-between mx-auto">
-			<div>
-				<a href="/" class="flex-1 text-2xl font-bold">
+		<nav class="max-w-5xl w-full px-6 h-[72px] flex items-center justify-between mx-auto">
+			<div class="flex-1">
+				<a href="/" class="text-2xl font-bold">
 					SolidTyper
 				</a>
 			</div>
-			<div class="flex-1 flex items-center lg:justify-end gap-6">
+			<div class="flex-1 flex items-center justify-end gap-6">
 				<a href="/settings" class="text-xl">
 					<SettingsIcon />
 				</a>
